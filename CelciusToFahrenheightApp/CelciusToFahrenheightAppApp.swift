8a4ab3cf7e5f8cf93f9bef55436c1dddc8c0f9f4
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-01-17 12:25:49 -0500

    Initial Commit

---
+++ CelciusToFahrenheightApp/CelciusToFahrenheightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelciusToFahrenheightAppApp.swift
+//  CelciusToFahrenheightApp
+//
+//  Created by Oliver Finlayson on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct CelciusToFahrenheightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
